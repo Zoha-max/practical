7c57d60dfbc134db2ea7786ff22e1b350fedf60c
Zoha-max <127471377+Zoha-max@users.noreply.github.com>
2023-03-15 13:33:36 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,11 @@
+class Rectangle{
+public static void main(String args[]){
+int length=5;
+int width=6;
+System.out.println(length+" "+width);
+int Area=length*width;
+int Perimeter=2*(length+width);
+System.out.println("Area of Rectangle is:"+Area);
+System.out.println("Perimeter of Rectangle is:"+Perimeter);
+}
+}
