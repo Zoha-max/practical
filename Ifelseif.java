ef4304333cbb4bc7b5785ae8da48a7371a352934
Zoha-max <127471377+Zoha-max@users.noreply.github.com>
2023-03-17 15:14:08 +0530

    Add files via upload

---
+++ Ifelseif.java
@@ -0,0 +1,21 @@
+class Ifelseif{
+public static void main(String[] args){
+String name = "Zoha";
+if (name=="Meena")
+  {
+   System.out.println("Name is Meena");
+  }
+  else if (name=="Neha")
+  {
+   System.out.println("Name is Neha");
+  }
+  else if (name=="Ankit")
+  {
+   System.out.println("Name is Ankit");
+  }
+else
+{
+System.out.println(name);
+}
+}
+}
