04f6ebd4579bf2b3844d4a9a090c1c2d62bbe658
Zoha-max <127471377+Zoha-max@users.noreply.github.com>
2023-03-28 12:37:00 +0530

    Add files via upload

---
+++ Object_classEg.java
@@ -0,0 +1,12 @@
+package com;
+
+public class Object_classEg {
+	int Sl_no=2;
+	String name="Zoha";
+	public static void main(String[] args) {
+		Object_classEg obj=new Object_classEg();
+		System.out.println(obj.Sl_no);
+		System.out.println(obj.name);
+	}
+
+}
---
+++ Stu_info.java
@@ -0,0 +1,22 @@
+package com;
+
+public class Stu_info {
+	int Age=24;
+	String name="Zoha";
+	String Add="Pune";
+	int Age2=25;
+	String name2="Vishakha";
+	String Add2="Solapur";
+	int Age3=26;
+	String name3="Manaal";
+	String Add3="Aurangaaba";
+	public static void main(String[] args) {
+		Stu_info obj=new Stu_info();
+		System.out.println(obj.Age+" "+obj.name+" "+obj.Add);
+		System.out.println(obj.Age2+" "+obj.name2+" "+obj.Add2);
+		System.out.println(obj.Age3+" "+obj.name3+" "+obj.Add3);
+	}
+
+}
+
+
